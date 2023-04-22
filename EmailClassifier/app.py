0e157389ba7def4495896083e1f4b8620610114d
kunal pandya <61379632+Kunal2103@users.noreply.github.com>
2023-04-22 18:44:11 +0530

    Update app.py

--- EmailClassifier/app.py
+++ EmailClassifier/app.py
@@ -23,8 +23,8 @@ def transform_text(text):
     for i in text:
         y.append(ps.stem(i))
     return " ".join(y)
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('https://github.com/Kunal2103/SMS-Spam-Not-Spam-Classification/tree/main/EmailClassifier/vectorizer.pkl','rb'))
+model = pickle.load(open('https://github.com/Kunal2103/SMS-Spam-Not-Spam-Classification/tree/main/EmailClassifier/model.pkl','rb'))
 st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
