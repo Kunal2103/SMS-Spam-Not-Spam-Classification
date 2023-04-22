4e6bad4c8effb6ed5065212cb621f1069532fbb1
kunal pandya <61379632+Kunal2103@users.noreply.github.com>
2023-04-22 19:01:28 +0530

    Update app.py

--- EmailClassifier/app.py
+++ EmailClassifier/app.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-import joblib
+import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
@@ -23,8 +23,8 @@ def transform_text(text):
     for i in text:
         y.append(ps.stem(i))
     return " ".join(y)
-tfidf = joblib.load(open('vectorizer.pkl','rb'))
-model = joblib.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
